c064a062cac52eee065eb77deb30dd11611cfda6
mesafnt <mesafnt.kalu@ucdenver.edu>
2023-03-19 00:26:53 -0600

    commit 2

--- Movies.js
+++ Movies.js
@@ -1,11 +1,30 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var bcrypt = require('bcrypt-nodejs');
 
 mongoose.connect(process.env.DB);
 
 // Movie schema
 var MovieSchema = new Schema({
+    Title: {type: String, required: true},
+    YearReleased: {type: String, required: true},
+    genre: {
+        type: String,
+        enum: ['Action', 'Adventure', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Mystery', 'Thriller'],
+    },
+    //add validation to pass at least three actors
+    Actors: [
+        {ActorName: {type: String, required: true}, CharacterName: {type: String, required: true}},
+        {ActorName: {type: String, required: true}, CharacterName: {type: String, required: true}},
+        {ActorName: {type: String, required: true}, CharacterName: {type: String, required: true}}
+    ]
+    /*
+    Movie.findOneAndUpdate(req.body.find_Title, req.body.update_Title, function(err, movie)
+    */
+});
 
+MovieSchema.pre('save', function(next) {
+    next();
 });
 
 // return the model
