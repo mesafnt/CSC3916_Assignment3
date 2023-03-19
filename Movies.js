cd33aac49a687aaf2a351f8fdd64c7cde9f54476
mesafnt <mesafnt.kalu@ucdenver.edu>
2023-03-19 01:48:52 -0600

    comm

--- Movies.js
+++ Movies.js
@@ -1,16 +1,10 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-var bcrypt = require('bcrypt-nodejs');
+//var bcrypt = require('bcrypt-nodejs');
 
-mongoose.Promise = global.Promise;
+mongoose.connect(process.env.DB);
 
-//mongoose.connect(process.env.DB, { useNewUrlParser: true });
-try {
-    mongoose.connect( process.env.DB, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true}, () =>
-        console.log("connected"));
-}catch (error) {
-    console.log("could not connect");
-}
+
 
 // Movie schema
 var MovieSchema = new Schema({
